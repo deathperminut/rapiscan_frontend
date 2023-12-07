ea3a889170a3c7cbb30cd36bfcbc0c912a9751f3
Sebastian Mendez <mendezsebas8@gmail.com>
2023-12-06 22:49:41 -0500

    "Cambios"

--- src/Router/Lobby/Lobby.jsx
+++ src/Router/Lobby/Lobby.jsx
@@ -9,6 +9,7 @@ import Airplane from '../../Components/Svg/Airplane/Airplane'
 import Mirror from '../../img/background.png'
 import Swal from 'sweetalert2'
 import Logo_team from '../../img/Logo_team.png'
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function Lobby() {
   /* NAVIGATE */
@@ -21,9 +22,35 @@ export default function Lobby() {
     });
   }
 
+  const seeDropDown=()=>{
+    document.getElementById('nav-dropdown-dark-example').click();
+  }
+
+  const  Lobby=()=>{
+    navigate('/Lobby')
+  }
+
+  const  LogOut=()=>{
+    navigate('/Login')
+  }
+
 
   return (
     <div className='Body_2' style={{backgroundImage: `url(${Fondo})`,backgroundSize:'cover'}}>
+          <div className='Navbar_2'>
+              <div onClick={seeDropDown} className='nameContainer dropdown-toggle d-flex flex-row justify-content-center align-items-center align-self-center'>
+                  <span className='nameNavbar font_medium'>
+                  Juan Sebastian Mendez
+                  </span>
+                  <NavDropdown
+                    id="nav-dropdown-dark-example"
+                    title=""
+                    menuVariant="dark"
+                  >
+                    <NavDropdown.Item onClick={LogOut}>Cerrar Sesión</NavDropdown.Item>
+                  </NavDropdown>
+              </div>
+          </div>
           <div className='logoContainer'>
               <Icon></Icon>
           </div>
